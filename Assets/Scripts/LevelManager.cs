02c5f3fb270985f0f1eafb7c1031b14ca059e93e
Willowww-oop <darkslayer1037@gmail.com>
2025-08-09 22:56:20 -0600

    Added a pause menu

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
+using UnityEngine.UIElements;
 
 public class LevelManager : MonoBehaviour
 {
     public string[] sceneName;
+
+    public static bool isGamePaused = false;
+
+    [SerializeField] GameObject pauseCanvas;
 
     public void OnStartLoad()
     {
@@ -24,6 +30,44 @@ public class LevelManager : MonoBehaviour
     public void OnTitleLoad()
     {
         SceneManager.LoadScene(sceneName[3], LoadSceneMode.Single);
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!isGamePaused)
+            {
+                OnPause();
+            }
+
+            else
+            {
+                OnResume();
+            }
+        }
+    }
+
+    public void OnPause()
+    {
+        isGamePaused = true;
+        
+        Time.timeScale = 0.0f;
+        
+        pauseCanvas.SetActive(true);
+
+        UnityEngine.Cursor.lockState = CursorLockMode.None;
+    }
+
+    public void OnResume()
+    {
+        isGamePaused = false;
+
+        Time.timeScale = 1.0f;
+
+        pauseCanvas.SetActive(false);
+
+        UnityEngine.Cursor.lockState = CursorLockMode.Confined;
     }
 
     public void OnQuit()
