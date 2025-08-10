aa91c949ccae7c67effe7e575cc1025e5a802b33
RileyDoesGameDev <Rileyartstuff12@gmail.com>
2025-08-09 21:20:24 -0600

    Enemy Detection

---
+++ Assets/Scenes/Towers/Accelerator/Accelerator.cs
@@ -0,0 +1,76 @@
+using System;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Accelerator : BaseTower
+{
+    public SO_Accelerator Data;
+    CircleCollider2D CircleCollider2D;
+    private List<GameObject> enemyCollection;
+    private bool enemyInRange;
+    private void Awake()
+    {
+     
+        damage = Data.damage;
+        discription = Data.discription;
+        cost = Data.cost;
+        range = Data.range;
+        fireRate = Data.fireRate;
+    }
+
+    void Start()
+    {
+        enemyCollection = new List<GameObject>();
+       CircleCollider2D = GetComponent<CircleCollider2D>();
+        CircleCollider2D.radius = Data.range;
+
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (enemyInRange)
+        {
+            Fire();
+        }
+       
+      
+        Debug.Log(enemyCollection.Count);
+    }
+
+    protected override void Fire()
+    {
+        Debug.Log("Fire");
+
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            enemyInRange = true;
+            enemyCollection.Add(collision.gameObject);
+        }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            enemyCollection.Remove(collision.gameObject);
+        }
+    }
+
+
+
+
+
+
+
+}
